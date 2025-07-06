9fd3f70c4ccb7ef1933c6bdf395652fafab3e61c
iamernesto14 <eernesto211@gmail.com>
2025-07-06 17:57:45 +0000

    feat(auth): implement AuthGuard to protect admin routes

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,5 +1,22 @@
-import { CanActivateFn } from '@angular/router';
+// src/app/core/guards/auth.guard.ts
+import { inject } from '@angular/core';
+import { CanActivateFn, CanMatchFn, Router, UrlSegment } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+export const canActivateAdmin: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  return authService.isAuthenticated()
+    ? true
+    : router.createUrlTree(['/login']);
+};
+
+export const canMatchAdmin: CanMatchFn = (route, segments: UrlSegment[]) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  return authService.isAuthenticated()
+    ? true
+    : router.createUrlTree(['/login']);
 };
