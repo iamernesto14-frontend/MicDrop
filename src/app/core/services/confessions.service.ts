ddc4cc7393eb4e42605f4c1cae3937840e28c1f2
Mildred Naab <mwinkomnaab@gmail.com>
2025-07-11 17:35:07 +0000

    feat: Implement admin confessions component

--- src/app/core/services/confessions.service.ts
+++ src/app/core/services/confessions.service.ts
@@ -2,19 +2,24 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
+import { ConfessionResponse } from '../../models/confession-response.model';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConfessionsService {
-  private apiUrl = `${environment.apiUrl}/confessions`;
-
   constructor(private http: HttpClient) {}
 
   submitConfession(message: string): Observable<any> {
     const body = { message };
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
 
-    return this.http.post(this.apiUrl, body, { headers });
+    return this.http.post(`${environment.apiUrl}/confessions`, body, { headers });
+  }
+
+  getAllConfessions(): Observable<ConfessionResponse> {
+    return this.http.get<ConfessionResponse>(`${environment.apiUrl}/confessions`).pipe(
+    );
   }
 }
