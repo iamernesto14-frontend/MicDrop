ae73ea8b5aaab054d86335b938b451f584ea6963
iamernesto14 <eernesto211@gmail.com>
2025-07-13 05:06:51 +0000

    add playlist to home page

--- src/app/pages/playlists/playlists/playlists.component.ts
+++ src/app/pages/playlists/playlists/playlists.component.ts
@@ -1,19 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../../shared/components/header/header.component';
-import { Home, MessageSquare, List, Play } from 'lucide-angular';
 import { MobileAdminMenuComponent } from '../../../shared/components/mobile-admin-menu/mobile-admin-menu.component';
+import { PlaylistService } from '../../../core/services/playlist.service';
+import { Playlist } from '../../../models/playlist.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-playlists',
-  imports: [HeaderComponent, MobileAdminMenuComponent],
+  standalone: true,
+  imports: [
+    HeaderComponent,
+    MobileAdminMenuComponent,
+    CommonModule,
+  ],
   templateUrl: './playlists.component.html',
   styleUrl: './playlists.component.scss',
 })
-export class PlaylistsComponent {
+export class PlaylistsComponent implements OnInit {
+  playlists: Playlist[] = [];
+  loading = true;
+  error = '';
+
   menuItems = [
-    { label: 'Home', icon: Home, route: ['/'] },
-    { label: 'Episodes', icon: Play, route: ['/episodes'] },
-    { label: 'Playlists', icon: List, route: ['/playlists'] },
-    { label: 'Confess', icon: MessageSquare, route: ['/confessions'] },
+    { label: 'Home', icon: 'home', route: ['/'] },
+    { label: 'Episodes', icon: 'play', route: ['/episodes'] },
+    { label: 'Playlists', icon: 'list', route: ['/playlists'] },
+    { label: 'Confess', icon: 'message-square', route: ['/confessions'] },
   ];
+
+  constructor(private playlistService: PlaylistService) {}
+
+  ngOnInit(): void {
+    this.playlistService.getPlaylists().subscribe({
+      next: (res) => {
+        this.playlists = res.data?.data || res.data || [];
+        this.loading = false;
+      },
+      error: () => {
+        this.error = 'Failed to load playlists.';
+        this.loading = false;
+      },
+    });
+  }
 }
